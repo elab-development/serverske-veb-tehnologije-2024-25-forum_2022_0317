53035eea74cb4ae1db581a905a5e91f87e6723e4
AleksaM321 <161731279+AleksaM321@users.noreply.github.com>
2025-10-17 10:57:22 +0200

    Dodavanje factories-a i seeder-a

--- forum/database/seeders/DatabaseSeeder.php
+++ forum/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->admin()->create([
+            'name' => 'Forum Admin',
+            'email' => 'admin@forum.local',
+            'password' => Hash::make('password'),
+            'can_publish' => true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(3)->moderator()->create();
+
+        User::factory()->count(20)->create();
+        User::factory()->count(3)->blockedFromPublishing()->create();
+
+        $this->call([
+            ThemeSeeder::class,
+            PostSeeder::class,
+            LikeSeeder::class,
+        ]);
     }
 }
