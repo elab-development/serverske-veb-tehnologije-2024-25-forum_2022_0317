6f2ab98fbd9c51910566c9f97dbd74072c70d2ce
AleksaM321 <161731279+AleksaM321@users.noreply.github.com>
2025-10-20 14:52:25 +0200

    proba

--- forum/app/Providers/AuthServiceProvider.php
+++ forum/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
